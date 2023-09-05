428b0b3a26c588d7f7934b565d0e075fe71b82d5
Aurélien <aurelien.r35@gmail.com>
2023-09-05 09:12:32 +0200

    table event

--- app/(routes)/admin/(dashboard)/components/display-events.tsx
+++ app/(routes)/admin/(dashboard)/components/display-events.tsx
@@ -60,57 +60,75 @@ const DisplayEvents = ({
       {date ? (
         <div>
           <p className="mb-2 font-bold">{dateFormatter(new Date(date))}</p>
-          {dailyEvents.length === 0 ? (
-            <p> Aucun évènement pour cette date </p>
-          ) : (
+          <div className="border rounded-md">
             <Table className="w-full">
               <TableHeader>
                 <TableRow>
-                  <TableHead>Nom</TableHead>
-                  <TableHead>Description</TableHead>
-                  <TableHead>Profil du client</TableHead>
-                  <TableHead>Supprimer</TableHead>
+                  <TableHead className="bg-primary text-primary-foreground">
+                    Nom
+                  </TableHead>
+                  <TableHead className="bg-primary text-primary-foreground">
+                    {" "}
+                    Description
+                  </TableHead>
+                  <TableHead className="bg-primary text-primary-foreground">
+                    Profil du client
+                  </TableHead>
+                  <TableHead className="bg-primary text-primary-foreground">
+                    Supprimer
+                  </TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {dailyEvents.map((event) => (
-                  <TableRow key={event.id}>
-                    <TableCell>{event.name}</TableCell>
-                    <TableCell>{event.description}</TableCell>
-                    <TableCell>
-                      <div className="flex items-center justify-center">
-                        {event.userId ? (
-                          <Link
-                            href={`/admin/users/${event.userId}`}
-                            target="_blank"
+                {dailyEvents.length > 0 ? (
+                  dailyEvents.map((event) => (
+                    <TableRow
+                      className={"odd:bg-secondary even:bg-primary/50"}
+                      key={event.id}
+                    >
+                      <TableCell>{event.name}</TableCell>
+                      <TableCell>{event.description}</TableCell>
+                      <TableCell>
+                        <div className="flex items-center justify-center">
+                          {event.userId ? (
+                            <Link
+                              href={`/admin/users/${event.userId}`}
+                              target="_blank"
+                            >
+                              <ExternalLink />
+                            </Link>
+                          ) : (
+                            <CircleOff />
+                          )}
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <div className="flex items-center justify-center">
+                          <Button
+                            disabled={loading}
+                            variant="destructive"
+                            size="sm"
+                            onClick={() => {
+                              setEventId(event.id);
+                              setOpen(true);
+                            }}
                           >
-                            <ExternalLink />
-                          </Link>
-                        ) : (
-                          <CircleOff />
-                        )}
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center justify-center">
-                        <Button
-                          disabled={loading}
-                          variant="destructive"
-                          size="sm"
-                          onClick={() => {
-                            setEventId(event.id);
-                            setOpen(true);
-                          }}
-                        >
-                          <Trash className="w-4 h-4" />
-                        </Button>
-                      </div>
+                            <Trash className="w-4 h-4" />
+                          </Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={4} className="text-center">
+                      {"Pas d'evenements"}
                     </TableCell>
                   </TableRow>
-                ))}
+                )}
               </TableBody>
             </Table>
-          )}
+          </div>
         </div>
       ) : (
         <p> Veuillez selectionner une date </p>
