790f9ac47641ffb1f56fa572fd4d83da879a073b
Aurélien <aurelien.r35@gmail.com>
2023-09-12 16:05:17 +0200

    fix

--- app/api/throw-error/route.ts
+++ app/api/throw-error/route.ts
@@ -5,7 +5,7 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { userId: string } }
 ) {
-  if (!params.userId) {
+  if (!params) {
     return new NextResponse("Internal error", {
       status: 500,
     });
