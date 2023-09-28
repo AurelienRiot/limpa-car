86dc2d974caefcfbb6f81519d32ae19a394530c9
Aurélien <aurelien.r35@gmail.com>
2023-09-28 11:58:27 +0200

    light

--- app/(routes)/(public)/car-show/car-show.tsx
+++ app/(routes)/(public)/car-show/car-show.tsx
@@ -35,7 +35,7 @@ export function CarShow() {
 
       <spotLight
         color={[1, 0.25, 0.7]}
-        intensity={2}
+        intensity={200}
         angle={0.6}
         penumbra={0.5}
         position={[5, 5, 0]}
@@ -45,7 +45,7 @@ export function CarShow() {
 
       <spotLight
         color={[0.14, 0.5, 1]}
-        intensity={5}
+        intensity={500}
         angle={0.6}
         penumbra={0.5}
         position={[-5, 5, 0]}
@@ -53,15 +53,7 @@ export function CarShow() {
         shadow-bias={-0.0001}
       />
       <Rings />
-      {/* <CubeCamera resolution={256} frames={Infinity}>
-{(texture) => (
-    <mesh>
- <meshStandardMaterial envMap={texture} />
-           <Car />
-    </mesh>
-  )}
-       
-      </CubeCamera> */}
+
       <Car />
       <Boxes />
       <Ground />
